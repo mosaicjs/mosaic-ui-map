beb2fead8266b3279873a91d486de1171829bae2
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-08-13 17:03:44 +0200

    Add leaflet adapters

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import InteractionLayer from './lib/Leaflet.InteractionLayer';
+import LeafletAdapter from './lib/LeafletAdapter';
+import TilesInfo from './lib/TilesInfo';
+import LeafletTilesAdapter from './lib/LeafletTilesAdapter';
+import DataSetLeafletAdapter from './lib/DataSetLeafletAdapter';
+import DataSetLeafletLayer from './lib/DataSetLeafletLayer';
 import Map from './lib/Map';
+import MapView from './lib/MapView';
 import MapViewport from './lib/MapViewport';
 import UtfGrid from './lib/Leaflet.UtfGrid';
+import registerAdapters from './lib/registerAdapters';
 
 export default {
+    DataSetLeafletAdapter,
+    DataSetLeafletLayer,
     InteractionLayer,
+    LeafletAdapter,
+    LeafletTilesAdapter,
     Map,
+    MapView,
     MapViewport,
-    UtfGrid
+    TilesInfo,
+    UtfGrid,
+    
+    registerAdapters
 }
