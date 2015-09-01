3d3ed7bb451e50f1e75f767389bc76ba8c00560b
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-09-01 20:19:01 +0200

    Update the styles.js file

--- styles.js
+++ styles.js
@@ -1,2 +1,3 @@
-
-import './lib/MapView.less';
+import "leaflet/dist/leaflet.css";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
